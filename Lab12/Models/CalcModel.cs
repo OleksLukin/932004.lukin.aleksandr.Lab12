14ae54c8d0110ad8c198c907dbb058ca09113dd9
Your Name <you@example.com>
2023-01-16 20:07:16 +0700

    Добавьте файлы проекта.

---
+++ Lab12/Models/CalcModel.cs
@@ -0,0 +1,14 @@
+﻿using Lab12.Services;
+namespace Lab12.Models
+{
+    public class CalcModel
+    {
+        public int a { get; set; }
+        public int b { get; set; }
+        public string operation { get; set; }
+        public string? GetValue()
+        {
+            return CalcService.Operation(a, b, operation);
+        }
+    }
+}
